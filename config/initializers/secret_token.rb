184367a5a1133ea2c41a818a66d56aece214d6e2
maxime <maxime.raverdy@gmail.com>
2011-12-10 01:28:58 +0100

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Permismoto::Application.config.secret_token = '471170f1d652eb9be0f00e9d2b80b173f7df6c8ffc24c0935521fa33e5a94eed7449e380772eea98b33cf1064a7ce559dfcd8ac23f340c7ff7001b4ef5bedeab'
